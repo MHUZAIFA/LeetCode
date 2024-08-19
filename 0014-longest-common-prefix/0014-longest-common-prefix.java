f82bfc3b16bbef9c011302e67be9522b74c23d61
Mohammed Huzaifa <mhuzaifa.career@outlook.com>
2024-08-19 21:21:44 +0530

    Time: 1 ms (73.49%), Space: 41.2 MB (87.59%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -1,25 +1,21 @@
 class Solution {
     public String longestCommonPrefix(String[] strs) {
-        if (strs.length == 1) return strs[0];
+        int numberOfWords = strs.length;
+
+        if (numberOfWords == 1) return strs[0];
+
         StringBuilder result = new StringBuilder();
         String refString = strs[0];
+        int refStringLength = refString.length();
 
         int i,j;
         boolean isFinalBreak = false;
-
-        for (i = 0; i < refString.length(); i++) {
+        for (i = 0; i < refStringLength; i++) {
             char currentChar = refString.charAt(i);
-            for (j = 1; j < strs.length; j++) {
-                if (i > strs[j].length() - 1) {
-                    isFinalBreak = true;
-                    break;
-                };
-
-                char charToCompare = strs[j].charAt(i);
-                if (currentChar != charToCompare) {
-                    isFinalBreak = true;
-                    break;
-                }
+            for (j = 1; j < numberOfWords; j++) {
+                if (i <= strs[j].length() - 1 && currentChar == strs[j].charAt(i)) continue;
+                isFinalBreak = true;
+                break;
             }
             if (isFinalBreak) break;
             result.append(currentChar);
